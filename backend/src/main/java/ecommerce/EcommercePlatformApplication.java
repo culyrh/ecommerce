71d462a0169c0aeabd26d81c85af0a6284d70cba
ubuntu <ict_701@naver.com>
2026-01-11 01:34:38 +0900

    fix: 이벤트 빌드 오류 수정
    - @Async를 같은 클래스 내에서 호출하여 프록시가 적용되지 않음
    - 비동기 처리를 외부 빈으로 분리

--- backend/src/main/java/ecommerce/EcommercePlatformApplication.java
+++ backend/src/main/java/ecommerce/EcommercePlatformApplication.java
@@ -2,10 +2,12 @@ package ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling  // 스케줄러 활성화를 위해 추가
+@EnableAsync       // 비동기 처리 활성화
 public class EcommercePlatformApplication {
 
 	public static void main(String[] args) {
